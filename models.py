2e9ebb33ab1c8571c900d994d8747c706de6dca7
J4CKINS <50251721+J4CKINS@users.noreply.github.com>
2020-11-10 11:59:19 +0000

    added blog post model

--- models.py
+++ models.py
@@ -1,1 +1,8 @@
 from app import db
+from datetime import datetime
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
