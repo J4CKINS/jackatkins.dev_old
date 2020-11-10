803a3656ab642855996686432ca45738be107ea8
J4CKINS <electronicjack4@gmail.com>
2020-11-10 20:07:19 +0000

    implemented admin login system

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import bcrypt
 
 from flask import Flask
 from flask import render_template
@@ -13,10 +14,22 @@ app = Flask(__name__)
 
 #SQLALCHEMY
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 #MODELS
+class AdminAccount(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(100))
+
 class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
+
+class ProjectPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created = db.Column(db.DateTime)
     title = db.Column(db.String(100))
@@ -43,15 +56,38 @@ def projects():
 
 @app.route("/admin/")
 def admin():
-    return render_template("admin.html")
+    try:
+        if session["admin_user"]:
+            return render_template("admin.html")
+    except KeyError:
+        return redirect(url_for("home"))
 
 @app.route("/admin/login/", methods=["GET","POST"])
 def login():
-    if request.method == "GET":
-        return render_template("login.html")
+    if request.method == "POST":
+        
+        # get data from form
+        username = request.form["user"]
+        password = request.form["pass"]
+
+        # get username and password from database
+        account = AdminAccount.query.filter_by(username=username).first()
+
+        # if account name not found
+        if account is None:
+            return redirect(url_for("home")) # redirect user
+
+        # compare entered password and account password
+        if bcrypt.checkpw(password.encode(), account.password.encode()):
+            #login successful
+            session["admin_user"] = account.username
+            return redirect(url_for("admin"))
+
+        else:
+            return redirect(url_for("home"))
+
     else:
-        # GET FORM DATA AND STUFF
-        pass
+        return render_template("login.html")
 
 @app.route("/admin/blog/newpost/", methods=["GET","POST"])
 def newblogpost():
