eb623712e486c572bdf1ab9993c83a45c20802f1
J4CKINS <electronicjack4@gmail.com>
2020-11-14 11:58:33 +0000

    app.py py deletd for some reason

---
+++ app.py
@@ -0,0 +1,356 @@
+from datetime import datetime
+import bcrypt
+import requests
+import json
+
+from flask import Flask
+from flask import render_template
+from flask import url_for
+from flask import redirect
+from flask import session
+from flask import request
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+#SQLALCHEMY
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+#MODELS
+class AdminAccount(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(100))
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
+
+class ProjectPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
+
+#APP CONFIG
+app.secret_key = "SN1KT4196419662003"
+
+# MAIN ROUTES
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/blog/")
+def blog():
+
+    # get blog posts from database
+    posts = BlogPost.query.all()
+
+    return render_template("blog.html", posts=posts)
+
+@app.route("/projects/")
+def projects():
+
+    # get posts from database
+    posts = ProjectPost.query.all()
+
+    return render_template("projects.html", posts=posts)
+
+
+#ADMIN PAGE ROUTES
+
+@app.route("/admin/")
+def admin():
+    try:
+        if session["admin_user"]:
+            return render_template("admin.html")
+    except KeyError:
+        return redirect(url_for("login"))
+
+
+@app.route("/admin/login/", methods=["GET","POST"])
+def login():
+
+    # LOGIN REQUEST
+    if request.method == "POST":
+        
+        # get data from form
+        username = request.form["user"]
+        password = request.form["pass"]
+
+        # get username and password from database
+        account = AdminAccount.query.filter_by(username=username).first()
+
+        # if account name not found
+        if account is None:
+            return redirect(url_for("home")) # redirect user
+
+        # compare entered password and account password
+        if bcrypt.checkpw(password.encode(), account.password.encode()):
+            #login successful
+            session["admin_user"] = account.username
+            return redirect(url_for("admin"))
+
+        else:
+            return redirect(url_for("home"))
+
+    else:
+        return render_template("login.html")
+        
+
+# BLOG
+@app.route("/admin/blog/posts/new/", methods=["GET","POST"])
+def newblogpost():
+
+    if request.method == "POST":
+        
+        # validate user
+        try:
+            if session["admin_user"]:
+                
+                # get form data
+                title = request.form["title"]
+                content = request.form["content"]
+
+                # add blog post to database
+                post = BlogPost(created=datetime.now(), title=title, content=content)
+                db.session.add(post)
+                db.session.commit()
+
+                # redirect to the blog page
+                return redirect(url_for("blog"))
+
+        except KeyError:
+            return redirect(url_for("login"))
+
+    else:
+        # check if admin is logged in
+        try:
+            if session["admin_user"]:
+                return render_template("newblogpost.html")
+        except KeyError:
+            return redirect(url_for("login"))
+
+
+
+@app.route("/admin/blog/posts/")
+def blogposts():
+    
+    #validate user
+    try:
+        if session["admin_user"]:
+            
+            #load posts from database
+            posts = BlogPost.query.all()
+
+            return render_template("blogposts.html", posts=posts)
+
+    except KeyError:
+        return redirect(url_for("login"))
+
+
+@app.route("/admin/blog/posts/edit/<id>/", methods=["GET", "POST"])
+def editblogpost(id):
+
+    #validate user
+    try:
+        if session["admin_user"]:
+
+            if request.method == "POST":
+                
+                # get form data
+                title = request.form["title"]
+                content = request.form["content"]
+
+                # get record that needs to be updated
+                post = BlogPost.query.filter_by(id=id).first()
+
+                if post:
+                    post.title = title
+                    post.content = content
+                    db.session.commit()
+
+                    return redirect(url_for("blogposts"))
+                
+                else:
+                    return redirect(url_for("blogposts"))
+            
+            else:
+                
+                #load post data from database
+                post = BlogPost.query.filter_by(id=id).first()
+
+                # if post exists
+                if post:
+                    return render_template("editblogpost.html", post=post)
+                else:
+                    return redirect(url_for("blogposts"))
+
+    except KeyError:
+        return redirect(url_for("login"))
+
+
+@app.route("/admin/blog/posts/delete/<id>/")
+def deleteblogpost(id):
+
+    # validate user
+    try:
+        if session["admin_user"]:
+
+            # get record from database
+            post = BlogPost.query.filter_by(id=id).first()
+
+            if post:
+                # delete post
+                db.session.delete(post)
+                db.session.commit()
+
+                return redirect(url_for("blogposts"))
+
+            else:
+                return redirect(url_for("blogposts"))
+
+    except:
+        return "403"
+
+
+# PROJECTS
+@app.route("/admin/projects/posts/new/", methods=["GET", "POST"])
+def newprojectpost():
+
+    # validate user
+    try:
+        if session["admin_user"]:
+
+            if request.method == "POST":
+
+                # get form data
+                title = request.form["title"]
+                content = request.form["content"]
+
+                post = ProjectPost(title=title, content=content, created=datetime.now())
+
+                db.session.add(post)
+                db.session.commit()
+
+                return redirect(url_for("projects"))
+
+            else:
+                return render_template("newprojectpost.html")
+    
+    except KeyError:
+        return redirect(url_for("login"))
+
+
+@app.route("/admin/projects/posts/")
+def projectposts():
+
+    # validate user
+    try:
+        if session["admin_user"]:
+            
+            # load posts from database
+            posts = ProjectPost.query.all()
+
+            return render_template("projectposts.html", posts=posts)
+    
+    except KeyError:
+        return redirect(url_for("login"))
+
+
+@app.route("/admin/projects/posts/edit/<id>", methods=["GET", "POST"])
+def editprojectpost(id):
+
+    # validate user
+    try:
+        if session["admin_user"]:
+
+            if request.method == "POST":
+                
+                # get form data
+                title = request.form["title"]
+                content = request.form["content"]
+
+                # get original post
+                post = ProjectPost.query.filter_by(id=id).first()
+
+                if post:
+                    post.title = title
+                    post.content = content
+
+                    # save new post
+                    db.session.commit()
+
+                    return redirect(url_for("projectposts"))
+
+                else:
+                    return redirect(url_for("projectposts"))
+
+            else:
+                # load post data from database
+                post = ProjectPost.query.filter_by(id=id).first()
+
+                if post:
+                    return render_template("editprojectpost.html", post=post)
+
+                else:
+                    return redirect(url_for("projectposts"))
+
+    except KeyError:
+        return redirect(url_for("login"))
+
+
+@app.route("/admin/projects/posts/delete/<id>")
+def deleteprojectpost(id):
+
+    # validate user
+    try:
+        if session["admin_user"]:
+
+            # get post from database
+            post = ProjectPost.query.filter_by(id=id).first()
+
+            if post:
+                db.session.delete(post)
+                db.session.commit()
+
+                return redirect(url_for("projectposts"))
+
+            else:
+                return redirect(url_for("projectposts"))
+
+    except KeyError:
+        return redirect(url_for("login"))
+
+
+@app.route("/admin/imageupload/", methods=["POST"])
+def imageupload():
+
+    #verify user
+    try:
+        if session["admin_user"]:
+            
+            #send data off to imgur api
+            payload = {"image": request.data.decode()}
+            headers = {'Authorization': 'Client-ID 78182536f30f0fe'}
+            res = requests.request("POST", "https://api.imgur.com/3/image", headers=headers, data=payload, files=[])
+
+            res = json.loads(res.text.encode('utf8').decode())
+
+            if res["success"]:
+                return res["data"]["link"]
+            else:
+                return res["success"] + " Error connecting to API"
+
+    except KeyError:
+        return "403"
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run()
+
