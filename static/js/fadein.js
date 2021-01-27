844035feb5e6883f4d8ad56976076992675adc96
J4CKINS <electronicjack4@gmail.com>
2021-01-27 23:45:47 +0000

    cleared master branch

--- static/js/fadein.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-    console.log(document.getElementsByClassName("fadein").item(0))
-    document.getElementsByClassName("fadein").item(0).style.opacity = "1"
-    document.getElementsByClassName("fadein").item(0).style.paddingTop = "0"
-}
