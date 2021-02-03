300c4f9ca89c44a6f50467c186d088420eddf843
J4CKINS <electronicjack4@gmail.com>
2021-02-03 11:32:20 +0000

    implemented mobile nav bar

---
+++ static/js/mobile_nav_controller.js
@@ -0,0 +1,10 @@
+const nav = document.getElementById("mobnav");
+const frame = nav.children.item(0);
+
+function openNav() {
+    nav.style.display = "block"
+}
+
+function closeNav() {
+    nav.style.display = "none";
+}
