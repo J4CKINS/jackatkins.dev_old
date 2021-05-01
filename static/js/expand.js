488bb105a9a80dcc4f1949b8d8b116982cd7b4fd
J4CKINS <electronicjack4@gmail.com>
2021-05-01 12:00:23 +0100

    Added setExpandControls function

--- static/js/expand.js
+++ static/js/expand.js
@@ -1,6 +1,24 @@
+function setExpandControls() {
+    const max_height = 500;
+    var posts = document.getElementsByClassName("post-container");
+    for (let element of posts) {
+        let expand = element.getElementsByClassName("expand")[0]
+        let post_object = element.getElementsByClassName("post")[0];
+        if(element.offsetHeight < max_height) {
+            // delete expand element as it is not needed
+            expand.remove();
+        }
+        else {
+            post_object.style.height = max_height.toString() + "px"
+        }
+    }
+}
+
 function expand(id) {
+    
     var post = document.getElementById("blog-" + id);
     var text = document.getElementById("expand-" + id);
+
     if (post.style.height === "auto") {
         post.style.height = "500px";
         text.innerText = "Show More";
