05a09047ea239b87ce4e365c0fd610e4d280a792
J4CKINS <electronicjack4@gmail.com>
2021-05-12 13:33:33 +0100

    Moved database class to seperate file

---
+++ database.py
@@ -0,0 +1,30 @@
+import mysql.connector
+class Database:
+
+    database = None
+    cursor = None
+
+    @staticmethod
+    def connect():
+        Database.database = mysql.connector.connect(
+            host="jackatkins.dev",
+            user="app",
+            password="xLmNN^&099nm>",
+            auth_plugin="mysql_native_password",
+            database="jackatkins_dev",
+            autocommit=True,
+        )
+        Database.cursor = Database.database.cursor()
+
+    @staticmethod
+    def disconnect():
+        if Database.database:
+            Database.database.close()
+            Database.cursor.close()
+
+    @staticmethod
+    def formatDatestamp(datestamp):
+        day     = datestamp.strftime("%d")
+        month   = datestamp.strftime("%m")
+        year    = datestamp.strftime("%Y")
+        return day, month, year
