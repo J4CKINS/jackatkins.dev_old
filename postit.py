b0df59f5510c8756ab5ef2a2ce36ed48cb022e4a
J4CKINS <electronicjack4@gmail.com>
2021-05-15 15:15:42 +0100

    Added routing for dashboard

--- postit.py
+++ postit.py
@@ -24,10 +24,33 @@ postit = Blueprint(
 
 @postit.route("/")
 def home():
+    return redirect(url_for('postit.dashboard', postType='blog'))
+
+@postit.route("/dashboard/<postType>/")
+def dashboard(postType):
     
     if 'auth' in session and 'id' in session:
         if Guard.authenticateUserToken(session['id'], session['auth']):
-            return render_template('postit/dashboard.html')
+            
+            # Get the blog posts from database
+            if postType == "blog":
+                posts = Database.getBlogPosts()
+
+            # Get the project posts from the database
+            elif postType == "projects":
+                posts = Database.getProjectPosts()
+            
+            # Something else has been entered in as postType
+            else:
+                # Try to guess what post type the user wants by looking at the first letter of url post type
+                if postType[0].lower() == "p":
+                    # if postType starts with char: p, return project posts dashboard
+                    return redirect(url_for('postit.dashboard', postType='projects'))
+                
+                # return blog posts by default
+                return redirect(url_for('postit.dashboard', postType='blog'))
+            
+            return render_template('postit/dashboard.html', posts=posts)
 
     return redirect(url_for("postit.login"))
 
@@ -49,6 +72,6 @@ def login():
 
             
 
-            return redirect(url_for('postit.home'))
+            return redirect(url_for('postit.dashboard', postType='blog'))
 
-        return redirect(url_for("home")) # if user cannot be authenticated, redirect to home page
+        return redirect(url_for('home')) # if user cannot be authenticated, redirect to dashboard page
