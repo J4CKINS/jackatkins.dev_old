e2e3fa2fc5a3c8631f3cff44e3088ed826262fef
J4CKINS <electronicjack4@gmail.com>
2021-06-04 11:55:00 +0100

    Created post editor page

--- database.py
+++ database.py
@@ -55,9 +55,9 @@ class Database:
     def getBlogPosts(postedOnly=False):
         Database.connect()
         if postedOnly:
-            Database.cursor.execute('SELECT * FROM tblBlogPosts WHERE posted=1 ORDER BY datestamp DESC')
+            Database.cursor.execute('SELECT * FROM tblBlogPosts WHERE posted=1 ORDER BY id DESC')
         else:
-            Database.cursor.execute('SELECT * FROM tblBlogPosts ORDER BY datestamp DESC')
+            Database.cursor.execute('SELECT * FROM tblBlogPosts ORDER BY id DESC')
         
         data = Database.cursor.fetchall()
         Database.disconnect()
@@ -65,14 +65,74 @@ class Database:
         return data
 
     @staticmethod
+    def getBlogPostByID(ID):
+        Database.connect()
+        Database.cursor.execute('SELECT * FROM tblBlogPosts WHERE id=%s;', (ID,))
+        data = Database.cursor.fetchone()
+        Database.disconnect()
+        return data
+
+    @staticmethod
+    def createBlogPost(title, content, posted):
+        Database.connect()
+        Database.cursor.execute('INSERT INTO tblBlogPosts (title, content, datestamp, posted) VALUES (%s, %s, now(), %s);', (title, content, posted))
+        Database.disconnect()
+
+    @staticmethod
+    def updateBlogPost(ID, title, content, posted):
+        Database.connect()
+        Database.cursor.execute('UPDATE tblBlogPosts SET title=%s, content=%s, posted=%s WHERE id=%s;', (title, content, posted, ID))
+        Database.disconnect()
+
+    @staticmethod
+    def blogPostExists(ID):
+        Database.connect()
+        Database.cursor.execute('SELECT id FROM tblBlogPosts WHERE id=%s;', (ID,))
+        data = Database.cursor.fetchone()
+        Database.disconnect()
+        if data:
+            return True
+        return False
+
+    @staticmethod
     def getProjectPosts(postedOnly=False):
         Database.connect()
         if postedOnly:
-            Database.cursor.execute('SELECT * FROM tblProjectPosts WHERE posted=1 ORDER BY datestamp DESC')
+            Database.cursor.execute('SELECT * FROM tblProjectPosts WHERE posted=1 ORDER BY id DESC')
         else:
-            Database.cursor.execute('SELECT * FROM tblProjectPosts ORDER BY datestamp DESC')
+            Database.cursor.execute('SELECT * FROM tblProjectPosts ORDER BY id DESC')
         
         data = Database.cursor.fetchall()
         Database.disconnect()
 
         return data
+
+    @staticmethod
+    def getProjectPostByID(ID):
+        Database.connect()
+        Database.cursor.execute('SELECT * FROM tblProjectPosts WHERE id=%s;', (ID,))
+        data = Database.cursor.fetchone()
+        Database.disconnect()
+        return data
+
+    @staticmethod
+    def createProjectPost(title, content, posted):
+        Database.connect()
+        Database.cursor.execute('INSERT INTO tblProjectPosts (title, content, datestamp, posted) VALUES (%s, %s, now(), %s);', (title, content, posted))
+        Database.disconnect()
+
+    @staticmethod
+    def updateProjectPost(ID, title, content, posted):
+        Database.connect()
+        Database.cursor.execute('UPDATE tblProjectPosts SET title=%s, content=%s, posted=%s WHERE id=%s;', (title, content, posted, ID))
+        Database.disconnect()
+
+    @staticmethod
+    def projectPostExists(ID):
+        Database.connect()
+        Database.cursor.execute('SELECT id FROM tblProjectPosts WHERE id=%s;', (ID,))
+        data = Database.cursor.fetchone()
+        Database.disconnect()
+        if data:
+            return True
+        return False
--- postit.py
+++ postit.py
@@ -50,9 +50,122 @@ def dashboard(postType):
                 # return blog posts by default
                 return redirect(url_for('postit.dashboard', postType='blog'))
             
-            return render_template('postit/dashboard.html', posts=posts)
+            return render_template('postit/dashboard.html', posts=posts, postType=postType)
 
     return redirect(url_for("postit.login"))
+
+
+@postit.route("editor/<postType>/<postID>", methods=['GET', 'POST'])
+def editor(postType, postID):
+
+    #incorrect spelling in post type has ocurred
+    # this needs to be corrected
+    if postType != "projects" or postType != "blog":
+        if postType[0].lower() == "p": postType = "projects"
+        else: postType = "blog"
+
+    # Route has received a GET request
+    if request.method == "GET":
+        # Authenticate user
+        if 'auth' in session and 'id' in session:
+            if Guard.authenticateUserToken(session['id'], session['auth']):
+                # Check if post is new or being edited 
+
+                if postID != "0":
+                    # get post data
+                    if postType == "blog":
+                        post = Database.getBlogPostByID(postID)
+                    
+                    else:
+                        post = Database.getProjectPostByID(postID)
+                    
+                    # check if any data was found
+                    if post:
+                        title = post[1]
+                        content = post[2]
+                        posted = str(bool(post[4])).lower()
+                    
+                    # redirect to a new post if no data was found
+                    else:
+                        return redirect(url_for('postit.editor', postType=postType, postID=0))
+                
+                else:
+                    # if the post is new,
+                    # set the title, content, and posted variables to a default value
+                    title = ""
+                    content = ""
+                    posted = "false"
+                return render_template('postit/editor.html', title=title, content=content, posted=posted)
+        
+        # redirect to login screen if user is not authed
+        return redirect(url_for('postit.login'))
+
+    # Check if request is POST method
+    elif request.method == "POST":
+        
+        # Authenticate user
+        if 'auth' in session and 'id' in session:
+            if Guard.authenticateUserToken(session['id'], session['auth']):
+                
+                # Get form data
+                title = request.form['title']
+                content = request.form['content']
+                posted = request.form.get('posted', '0')
+                posted = '1' if posted == 'on' else '0'
+
+                print(title, content, posted)
+                
+                # Check post type   
+                if postType == "blog":
+
+                    # Check if post exists
+                    if not Database.blogPostExists(postID):
+                        postID = '0' # Set ID to 0 so a new post is created
+
+                    # Check if post is new
+                    if postID == '0':
+                        Database.createBlogPost(
+                            title,
+                            content,
+                            posted
+                        )
+
+                    # If post is not new, modify an existing post
+                    else:
+                        Database.updateBlogPost(
+                            postID,
+                            title,
+                            content,
+                            posted
+                        )
+
+                # Post type is project
+                else:
+
+                    # Check if post exists
+                    if not Database.projectPostExists(postID):
+                        postID = '0'
+
+                    # Check if post is new
+                    if postID == '0':
+                        Database.createProjectPost(
+                            title,
+                            content,
+                            posted
+                        )
+
+                    else:
+                        Database.updateProjectPost(
+                            postID,
+                            title,
+                            content,
+                            posted
+                        )
+
+                return redirect(url_for('postit.home'))
+                
+        return abort(403)
+
 
 @postit.route("/login/", methods=["GET", "POST"])
 def login():
