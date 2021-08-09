3bfc229c5e8f12acf3ca1a2e0a389ce1db43a5ce
J4CKINS <electronicjack4@gmail.com>
2021-08-09 22:50:37 +0100

    Test script now takes in host arg

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import sys
 from __init__ import app
 
 if __name__ == "__main__":
-    app.config['SERVER_NAME'] = f'jackatkins.test:{str(sys.argv[1])}'
+    app.config['SERVER_NAME'] = f'{sys.argv[1]}:{str(sys.argv[2])}'
     app.run(debug=True)
