6f7b87e55dbbace53e798767b2ef0c59df9f8beb
J4CKINS <jackatkins3@gmail.com>
2021-12-24 19:50:43 +0000

    Gallery: confirmation needed when deleting content

--- static/js/gallery/delete_folder.js
+++ static/js/gallery/delete_folder.js
@@ -1,6 +1,8 @@
 const deleteFolderRequest = new XMLHttpRequest();
 
 function delete_folder(path, name) {
+    let confirm = window.confirm("Are you sure you want to delete this folder?")
+    if (!confirm) { return false; }
     deleteFolderRequest.open("POST", "/gallery/delete/folder/" + path + (path != "" ? "/" : "") + name + "/");
     deleteFolderRequest.send();
 }
--- static/js/gallery/delete_image.js
+++ static/js/gallery/delete_image.js
@@ -1,6 +1,8 @@
 const deleteImageRequest = new XMLHttpRequest();
 
 function delete_image(path, image) {
+    let confirm = window.confirm("Are you sure you want to delete this image?")
+    if (!confirm) { return false; }
     console.log((path != ("") ? "/":"") + path + image)
     deleteImageRequest.open("POST", "/gallery/delete/image" + (path != ("") ? "/":"") + path + image);
     deleteImageRequest.send();
